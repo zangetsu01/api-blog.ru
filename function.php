2b645bb1456fb928b1a5cc6bd96baf7528740e72
satenov <Riting20@yandex.ru>
2026-03-26 12:07:34 +0300

    Работа с Преподователем

---
+++ function.php
@@ -0,0 +1,70 @@
+<?php
+
+function getPosts($pdo)
+{
+    $sql = "SELECT * FROM `posts`";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    $posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($posts);
+}
+
+function getPost($pdo, $id)
+{
+    $stmt = $pdo->prepare("SELECT * FROM posts WHERE id = :id");
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+
+    if($stmt->rowCount() === 1){
+ $post = $stmt->fetch(PDO::FETCH_ASSOC);
+ echo json_encode($post);
+
+    } else {
+        http_response_code(404);
+        $response = [
+            "status" => false,
+            "massage" => 'Post not found'
+        ];
+    echo json_encode($response);
+        }
+    }
+
+function addPost($pdo, $data)
+{
+    $sql = "INSERT INTO `posts`(`title`, `body`) VALUES (:title, :body)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($data);
+    http_response_code(201);
+    $response = [
+        "status" => true,
+        "post_id" => $pdo->lastInsertId()
+    ];
+    echo json_encode($response);
+
+}
+function updatePost($pdo, $id, $data){
+    $sql = "UPDATE `posts` SET `title` = :title, `body` = :body WHERE `id` = :id";
+    $stmt= $pdo->prepare($sql);
+    $data['id'] = $id;
+    $stmt->execute($data);
+    http_response_code(200);
+    $response = [
+        "status" => true,
+        "post_id" => $id
+    ];
+    echo json_encode($response);
+}
+
+function deletePost($pdo, $id) 
+{
+    $sql = "DELETE FROM posts WHERE id = :id";
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
+    http_response_code(200);
+    $response = [
+        'status' => true,
+        "mesend" => 'delete!'
+    ];
+    echo json_encode($response);
+}
---
+++ index.php
@@ -0,0 +1,51 @@
+
+
+<?php
+// http://api.blog.ru/posts.php
+
+header('Content-Type: application/json');
+require 'conectdb.php';
+require 'function.php';
+//die(print_r($_POST));
+
+// die(print_r($_SERVER));
+
+$method = $_SERVER['REQUEST_METHOD'];
+$params = explode('/', $_GET['q']);
+$type = $params[0];
+if (isset($params[1])) {
+    $id = $params[1];
+}
+
+
+switch ($method) {
+    case 'GET':
+        if ($type === 'posts'){
+            if (isset($id)){
+                //вывод одного поста
+                getPost($pdo, $id);
+            } else {
+                getPosts($pdo);
+            }
+        }
+        break;
+    case 'POST':
+        if ($type === 'posts'){
+            addPost($pdo, $_POST);
+        }
+        break;
+    case 'PATCH':
+        if ($type === 'posts'){
+            if (isset($id)){
+                $data = file_get_contents ('php://input');
+                $data = json_decode($data, true); 
+            // die($data['title']);
+                updatePost($pdo, $id, $data);
+            } 
+        }
+        break;
+        case 'DELETE':
+            if ($type === 'posts'){
+            deletePost($pdo, $id);
+        }
+}
